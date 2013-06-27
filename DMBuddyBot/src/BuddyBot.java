ee339f1b806aa59a4bc82a548ff9565ae43202f7
OtherwiseJunk <DartKietanmartaru@gmail.com>
2013-06-27 00:59:02 -0400

    Add Character Creation
    Added the ability to ask the DMBuddyBot to "Roll Honest Character 3d6".
    Implemented a check for a 4d6 character as well, but method to roll a
    4d6 variant character not yet implemented.

--- DMBuddyBot/src/BuddyBot.java
+++ DMBuddyBot/src/BuddyBot.java
@@ -9,12 +9,6 @@ public class BuddyBot extends PircBot {
         this.setName(Name);
 
     }
-
-    public void onJoin(String channel, String sender, String login, String hostname){
-    	
-    	sendMessage(channel, "Sup Mothafucka?!");
-    	
-    }
     
     public void onMessage(String channel, String sender, String login, String hostname, String message) {
         
@@ -39,6 +33,14 @@ public class BuddyBot extends PircBot {
 
         }
         
+        if (message.equalsIgnoreCase("Roll Honest Character 3d6")){
+        	honestChar(3, channel);
+        }
+        
+        if (message.equalsIgnoreCase("Roll Honest Character 4d6")){
+        	honestChar(4, channel);
+        }
+        
         if (message.equalsIgnoreCase("GTFO")) {
 
         	sendMessage(channel, "Fine, I'm sick of your shit anyway. Roll your own fucking dice.");
@@ -55,35 +57,36 @@ public class BuddyBot extends PircBot {
 
     public void onPrivateMessage(String channel, String sender, String login, String hostname, String message) {
     	
+    	//This method does not seem to be triggering correctly.
         if (message.startsWith("roll a ")) {
         	
-        	sendMessage(sender, "And your total roll is..."   );
-        	
-        	try {
-				Thread.sleep(250);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-        	
-        	sendMessage(sender, Roll(Integer.parseInt( message.substring(7,8) ), Integer.parseInt( message.substring(9,10) ) ) );
+        	sendMessage(sender, "You rolled a" + message.substring(7,10) + "and got " +  Roll(Integer.parseInt( message.substring(7,8) ), Integer.parseInt( message.substring(9,10) ) ) + "." );
 
         }
     }
     
-    public String Roll(int side, int times){
+    public String Roll(int times, int sides){
     	
-    	Dice dice = new Dice(side);
+    	Dice dice = new Dice(sides);
     	
     	return Integer.toString( dice.Roll(times) );	
     	
     }
     
-    public static Integer tryParse(String text) {
-    	  try {
-    	    return new Integer(text);
-    	  } catch (NumberFormatException e) {
-    	    return null;
-    	  }
-    	}
+    
+    //Method to roll an "honest" stat Char. User can specify the number of times they would like the dice rolled.
+    //Current method will only work correctly with 3d6 variant, need to add RollDrop method for 4d6 variant. Once this change is made add a check for the value of times.
+    public void honestChar(int times, String channel){
+    	
+    	Dice dice = new Dice(6);
+    	sendMessage(channel, "Strength: " + dice.Roll(3));
+    	sendMessage(channel, "Dexterity: " + dice.Roll(3));
+    	sendMessage(channel, "Constitution: " + dice.Roll(3));
+    	sendMessage(channel, "Wisdom: " + dice.Roll(3));
+    	sendMessage(channel, "Intelligence: " + dice.Roll(3));
+    	sendMessage(channel, "Charisma: " + dice.Roll(3));
+    	
+    }
+    
     
 }
